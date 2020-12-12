dee9ad65e9061a62ce7c470d1bff201ce5873c84
mdeeb2 <59974667+mdeeb2@users.noreply.github.com>
2020-12-12 16:31:42 -0500

    Update caloriecalculator.js

--- caloriecalculator.js
+++ caloriecalculator.js
@@ -1,8 +1,30 @@
-function calcAndShowTotal() {
-    var total = [].reduce.call($('#caloriesConsumed :checkbox:checked'), function(a, b) {
-      return a + +$(b).attr('calories') || 0;
-    }, 0);
-    $('#total').val(total);
+var calorieArray = new Array();
+calorieArray["margarita"] = 200;
+calorieArray["shrimp"] = 99;
+calorieArray["sweettea"] = 91;
+calorieArray["ribs"] = 299;
+calorieArray["burger"] = 354;
+calorieArray["pizza"] = 285;
+calorieArray["brisket"] = 270;
+calorieArray["corona"] = 270;
+calorieArray["coke"] = 140;
+
+function getCalorie() {
+  var Calorie = 0;
+  var theForm = document.forms["caloriesConsumed"];
+  var selectedCalories = theForm.elements["selectedCalories"];
+
+  for (var i = 0; i < selectedCalories.length; i++) {
+  	if(selectedCalories[i].checked){
+    	Calorie += calorieArray[selectedCalories[i].value] || 0;
+    }
   }
-  
-  $('#caloriesConsumed :checkbox').change(calcAndShowTotal).change();
+
+  return Calorie;
+}
+
+function getTotals() {
+  var totalCalories = getCalorie();
+  var totalCaloriesDIV = document.getElementById("totalCalories");
+  totalCaloriesDIV.innerText = "Total Calories: " + totalCalories;
+}
